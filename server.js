ec0fa39bdb82813b2af8fa41b4f83317b395d536
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-06-15 12:41:55 +0300

    initialize payments

---
+++ server.js
@@ -0,0 +1,45 @@
+require('dotenv').config
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.static('public'))
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+
+const storeItems = new Map([
+    [1, { priceInCents: 10000, name: "chair"}],
+    [2, { priceInCents: 20000, name: "table"}],
+])
+
+app.post('/create-checkout-session', async (req, res) => {
+    try{
+        const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        mode: 'payment',
+        line_items: req.body.items.map(item => {
+            const storeItem = storeItems.get(item.id)
+            return {
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: storeItem.name
+                    },
+                    unit_amount: storeItem.priceInCents
+                },
+                quantity: item.quantity
+            }
+        }),
+        success_url:`http://localhost:3000/success.html`,
+        cancel_url:`http://localhost:3000/cancel.html`
+        })
+        res.json({ url:session.url})
+    }catch (e){
+        res.status(500).json({error: e.message })
+    }
+    
+})
+
+app.listen(3000)
+
+// STRIPE_PRIVATE_KEY=sk_test_51LApQHJt9QgggVy71YelU0F3PHk7LNLnj2ygU4uaXhHFWdTZQHeXDQ9F0wOIEECO11o1cVMEdCLeXh48usm2VslO00SYneAItG
